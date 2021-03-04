2f74d59b3d62086dcc210550960dd149432f38fb
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-04 10:41:48 +0400

    Update StreamingVideoToHTTP.py

--- StreamingVideoToHTTP.py
+++ StreamingVideoToHTTP.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, Response
+from flask import Flask, Response
 import cv2
 from LVC_Camera import LegoVisionCommandCamera
 
@@ -10,11 +10,7 @@ read_task = camera.read()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World'
-
-def gen(read_task):
+def gen():
     while True:
         np_data = next(read_task)
         image = cv2.cvtColor(np_data, cv2.COLOR_BayerGR2RGB)
@@ -22,9 +18,9 @@ def gen(read_task):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(read_task),
+@app.route('/')
+def index():
+    return Response(gen(),
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
