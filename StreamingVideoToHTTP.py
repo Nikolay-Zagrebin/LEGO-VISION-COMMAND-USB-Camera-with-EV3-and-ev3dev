db2cabe62d9584fafdcc1cf29494652dd63ca281
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-04 12:53:42 +0400

    Update StreamingVideoToHTTP.py

--- StreamingVideoToHTTP.py
+++ StreamingVideoToHTTP.py
@@ -15,13 +15,11 @@ def gen():
         np_data = next(read_task)
         image = cv2.cvtColor(np_data, cv2.COLOR_BayerGR2RGB)
         ret, jpeg = cv2.imencode('.jpg', image)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 @app.route('/')
 def index():
-    return Response(gen(),
-        mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(host ="192.168.1.39")
