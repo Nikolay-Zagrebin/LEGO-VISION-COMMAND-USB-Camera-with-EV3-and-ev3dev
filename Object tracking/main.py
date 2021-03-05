d6a208ce4961409eef478c5e3c53957d898a33ba
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-05 19:31:25 +0400

    Create main.py

---
+++ Object tracking/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from flask import Flask, Response
+import ObjectTracking
+from ObjectTracking import TrackingObject
+
+TrObj = TrackingObject()
+
+app = Flask(__name__)
+
+def gen():
+    while True:
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + TrObj.gen_frame() + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host ="192.168.1.39")
