70e93e0a28f56f98f8ac697b7448dd573850c125
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-06 18:08:29 +0400

    Стрим видео потока в HTTP

--- StreamingVideoToHTTP.py
+++ StreamingVideoToHTTP.py
@@ -13,7 +13,6 @@ def gen():
     while True:
         image = cv2.cvtColor(camera.read(), cv2.COLOR_BayerGR2RGB)
         _, jpeg = cv2.imencode('.jpg', image)
-
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 @app.route('/')
