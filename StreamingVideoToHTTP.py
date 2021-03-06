5016bfc7e71813dc352e80f1c2e20d0f9d541992
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-06 17:47:09 +0400

    Update StreamingVideoToHTTP.py

--- StreamingVideoToHTTP.py
+++ StreamingVideoToHTTP.py
@@ -6,20 +6,19 @@ from LVC_Camera import LegoVisionCommandCamera
 
 camera = LegoVisionCommandCamera('/dev/video0')
 camera.start_capturing()
-read_task = camera.read()
 
 app = Flask(__name__)
 
 def gen():
     while True:
-        np_data = next(read_task)
-        image = cv2.cvtColor(np_data, cv2.COLOR_BayerGR2RGB)
-        ret, jpeg = cv2.imencode('.jpg', image)
+        image = cv2.cvtColor(camera.read(), cv2.COLOR_BayerGR2RGB)
+        _, jpeg = cv2.imencode('.jpg', image)
+
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 @app.route('/')
 def index():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')  
 
 if __name__ == "__main__":
     app.run(host ="192.168.1.39")
